f7574dae57ceb81c9d5af5ec6fa379f09ee027ab
Legenda-Lee <lee.legenda@gmail.com>
2020-04-17 14:15:37 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutInteger/Test1.java
@@ -0,0 +1,20 @@
+package com.legenda.lee.studytest.aboutInteger;
+
+/**
+ * @author Legenda-Lee
+ * @date 2020-04-17 14:13
+ * @description
+ * @since 1.0.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String a = "0003";
+        String b = "0043";
+        String c = "0403";
+
+        System.out.println(Integer.valueOf(a));
+        System.out.println(Integer.valueOf(b));
+        System.out.println(Integer.valueOf(c));
+
+    }
+}
