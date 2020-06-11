e0d576ec6741eaba62df8489b79caaeb8e744334
Legenda-Lee <lee.legenda@gmail.com>
2020-06-11 15:26:28 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutString/Test4.java
@@ -0,0 +1,20 @@
+package com.legenda.lee.studytest.aboutString;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @author Legenda-Lee
+ * @date 2020-06-10 10:56
+ * @description
+ * @since 1.0.0
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        String a = "hh ";
+        System.out.println(a.length());
+
+        String b = a.trim();
+        System.out.println(b);
+        System.out.println(b.length());
+    }
+}
