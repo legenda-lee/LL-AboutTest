85c6ae91fe735f9531bb78bf65202a2012ec5049
Legenda-Lee <dali@91jkys.com>
2020-09-24 18:39:18 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutString/TestContact.java
@@ -0,0 +1,14 @@
+package com.legenda.lee.studytest.aboutString;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2020-09-23 8:46 下午
+ * @description:
+ */
+public class TestContact {
+
+    public static void main(String[] args) {
+        String a = "rest";
+        System.out.println(a.concat("3").concat(null));
+    }
+}
