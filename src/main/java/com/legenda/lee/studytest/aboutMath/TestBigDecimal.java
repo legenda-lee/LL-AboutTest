697d50d0060206ff416ae41063b9df07a3623417
Legenda-Lee <dali@91jkys.com>
2020-10-31 14:13:05 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutMath/TestBigDecimal.java
@@ -0,0 +1,16 @@
+package com.legenda.lee.studytest.aboutMath;
+
+import java.math.BigDecimal;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2020-10-30 10:30 上午
+ * @description:
+ */
+public class TestBigDecimal {
+
+    public static void main(String[] args) {
+        // BigDecimal bigDecimal = new BigDecimal(null);
+        BigDecimal bigDecimal1 = new BigDecimal("");
+    }
+}
