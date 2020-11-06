b9a6363e2404043866213ab101fc0efe4eb9e44e
Legenda-Lee <dali@91jkys.com>
2020-11-06 16:45:11 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutSystem/Test1.java
@@ -0,0 +1,19 @@
+package com.legenda.lee.studytest.aboutSystem;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2020-11-06 2:12 下午
+ * @description:
+ */
+public class Test1 {
+
+    private static int a = 3;
+
+    static {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
