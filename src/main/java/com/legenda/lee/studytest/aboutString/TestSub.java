d8add12ffbf0df63f30f5ab1baa12869894a34d2
Legenda-Lee <dali@91jkys.com>
2021-02-01 10:40:34 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutString/TestSub.java
@@ -0,0 +1,15 @@
+package com.legenda.lee.studytest.aboutString;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2021-01-29 11:26 上午
+ * @description:
+ */
+public class TestSub {
+
+    public static void main(String[] args) {
+        String a = "汇总数据-";
+
+        System.out.println(a.substring(0, a.length() - 1));
+    }
+}
