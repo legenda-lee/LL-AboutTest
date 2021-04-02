2aaceb57d5907a799c9be8393bb90631e1cfd423
Legenda-Lee <lee.legenda@gmail.com>
2021-04-02 17:38:18 +0800

    json
    Signed-off-by: Legenda-Lee <lee.legenda@gmail.com>

--- src/main/java/com/legenda/lee/studytest/aboutString/TestSub.java
+++ src/main/java/com/legenda/lee/studytest/aboutString/TestSub.java
@@ -8,13 +8,16 @@ package com.legenda.lee.studytest.aboutString;
 public class TestSub {
 
     public static void main(String[] args) {
-        String a = "汇总数据-";
+        // String a = "汇总数据-";
+        //
+        // System.out.println(a.substring(0, a.length() - 1));
+        //
+        // System.out.println(1 << 1);
+        //
+        //
+        // System.out.println("yyyyMMddHHmmssSSS".substring(10));
 
-        System.out.println(a.substring(0, a.length() - 1));
-
-        System.out.println(1 << 1);
-
-
-        System.out.println("yyyyMMddHHmmssSSS".substring(10));
+        String aa = "1000200329DZSYAB";
+        System.out.println(aa.substring(0, 5) + "*****"+ aa.substring(10, 16));
     }
 }
