e7d1ae88e7b2d126367885563e1f202ad81d70b1
Legenda-Lee <dali@91jkys.com>
2021-04-12 16:58:15 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutString/Test6.java
@@ -0,0 +1,20 @@
+package com.legenda.lee.studytest.aboutString;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2021-04-12 3:14 下午
+ * @description:
+ */
+public class Test6 {
+
+    public static void main(String[] args) {
+        String multiSelect13 = "";
+
+        multiSelect13 = multiSelect13.substring(0, multiSelect13.length() - 1);
+
+        System.out.println(multiSelect13);
+
+    }
+
+
+}
