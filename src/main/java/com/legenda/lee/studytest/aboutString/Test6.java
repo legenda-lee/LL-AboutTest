6c400ec1f235c7b597c2dc52c6a5a333f888833a
Legenda-Lee <dali@91jkys.com>
2021-04-14 15:23:13 +0800

    sth

--- src/main/java/com/legenda/lee/studytest/aboutString/Test6.java
+++ src/main/java/com/legenda/lee/studytest/aboutString/Test6.java
@@ -1,5 +1,11 @@
 package com.legenda.lee.studytest.aboutString;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: Legenda-Lee(lee.legenda@gmail.com)
  * @date: 2021-04-12 3:14 下午
@@ -8,11 +14,20 @@ package com.legenda.lee.studytest.aboutString;
 public class Test6 {
 
     public static void main(String[] args) {
-        String multiSelect13 = "";
+        String multiSelect13 = "333";
 
         multiSelect13 = multiSelect13.substring(0, multiSelect13.length() - 1);
 
         System.out.println(multiSelect13);
+
+
+        String targetPromotionIds = "[34324432]";
+        List<Long> targetPromotionIdList = new ArrayList<>();
+        if (!StringUtils.isEmpty(targetPromotionIds)) {
+            targetPromotionIdList = JSON.parseArray(targetPromotionIds, Long.class);
+        }
+
+        System.out.println(JSON.toJSONString(targetPromotionIdList));
 
     }
 
