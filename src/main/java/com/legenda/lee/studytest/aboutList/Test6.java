a2a1a0259db2cbe2cf617ddd61a6d555f2d629a3
Legenda-Lee <dali@91jkys.com>
2021-06-02 16:30:50 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutList/Test6.java
@@ -0,0 +1,19 @@
+package com.legenda.lee.studytest.aboutList;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2021-06-02 4:23 下午
+ * @description: 两两组合
+ */
+public class Test6 {
+    public static void main(String[] args) {
+
+
+        for(int i = 1 ; i <= 3 ; i ++){
+            for(int j = i + 1 ; j <= 3 ; j ++){
+                System.out.println("I" + i + "I" +j + "   ");
+            }
+        }
+
+    }
+}
