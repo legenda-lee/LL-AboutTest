e7c9cbbcff4da64a269ceb2af9ac406e4e48c3e7
Legenda-Lee <dali@91jkys.com>
2021-06-17 12:49:10 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutMap/Test3.java
@@ -0,0 +1,21 @@
+package com.legenda.lee.studytest.aboutMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2021-06-17 11:05 上午
+ * @description:
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        Map<String, String> yktFeaturesMapOfIdCard = new HashMap<>();
+        yktFeaturesMapOfIdCard.put("name", "Legenda-Lee");
+        yktFeaturesMapOfIdCard.put("age", "0");
+        yktFeaturesMapOfIdCard.put("dd", "china");
+
+        System.out.println(yktFeaturesMapOfIdCard.get("3333"));
+    }
+}
