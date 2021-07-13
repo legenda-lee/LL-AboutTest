a08707833ee036b4ffa9203894971210b7694a23
Legenda-Lee <dali@91jkys.com>
2021-07-13 14:36:33 +0800

    sth

---
+++ src/main/java/com/legenda/lee/studytest/aboutString/Test7.java
@@ -0,0 +1,21 @@
+package com.legenda.lee.studytest.aboutString;
+
+/**
+ * @author: Legenda-Lee(lee.legenda@gmail.com)
+ * @date: 2021-04-12 3:14 下午
+ * @description:
+ */
+public class Test7 {
+
+    public static void main(String[] args) {
+
+
+        String aa = "佳节快乐.发多少";
+
+        System.out.println(aa.replaceAll(".", "hhhh"));
+        System.out.println(aa.replace(".", "hhhh"));
+
+    }
+
+
+}
