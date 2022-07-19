0648fac6370bd8abeaebdb7d81064bec97397373
Legenda-Lee <dali@91jkys.com>
2022-07-19 17:07:34 +0800

    add sth

--- src/main/java/com/legenda/lee/studytest/aboutEnum/Test2.java
+++ src/main/java/com/legenda/lee/studytest/aboutEnum/Test2.java
@@ -1,5 +1,8 @@
 package com.legenda.lee.studytest.aboutEnum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: Legenda-Lee(lee.legenda@gmail.com)
  * @date: 2020-11-03 2:27 下午
@@ -12,5 +15,11 @@ public class Test2 {
         // System.out.println(eventSceneEnum);
         PrescriptionResultTypeEnum resultTypeEnum = PrescriptionResultTypeEnum.getEnum(2);
         System.out.println(resultTypeEnum.name());
+
+        List<PrescriptionResultTypeEnum> list = new ArrayList<>();
+        list.add(PrescriptionResultTypeEnum.REFUSE);
+        list.add(PrescriptionResultTypeEnum.WARNING);
+
+        System.out.println(list.contains(PrescriptionResultTypeEnum.PASS));
     }
 }
