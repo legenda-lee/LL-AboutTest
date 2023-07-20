e77932611f2ea759ccf895c8dc3ddd41a38e808e
李三学 <lee.legenda@gmail.com>
2023-07-20 09:02:03 +0800

    add sth

--- src/main/java/com/legenda/lee/studyHutool/Test1.java
+++ src/main/java/com/legenda/lee/studyHutool/Test1.java
@@ -4,6 +4,8 @@ import cn.hutool.core.date.SystemClock;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.StrUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,15 +18,19 @@ public class Test1 {
 
 
         //Set<String> set = new HashSet<>();
-        //for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10000000; i++) {
         //    if (!set.add(generatorStrKey())) {
         //        System.out.println("重复了");
         //    }
-        //}
+            String a = getSerialNumber();
+            if (a.length() != 16) {
+                System.out.println(a);
+            }
+        }
 
-        Long a = 4234L;
-        System.out.println(a.toString());
-        System.out.println(String.valueOf(a));
+        //Long a = 4234L;
+        //System.out.println(a.toString());
+        //System.out.println(String.valueOf(a));
     }
 
 
@@ -34,5 +40,15 @@ public class Test1 {
         String now = String.valueOf(SystemClock.now()).substring(0, 8);
         String suffix = String.format("%010d", temp);
         return now + suffix;
+    }
+
+
+    public static String getSerialNumber() {
+        int hashCode = UUID.randomUUID().toString().hashCode();
+        if (hashCode < 0) {
+            hashCode = -hashCode;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        return sdf.format(new Date()).substring(2, 8) + String.format("%010d", hashCode);
     }
 }
